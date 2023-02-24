a23af28230102dd00556e982bcf00ea0faabd15e
fancystuff4 <68031547+fancystuff4@users.noreply.github.com>
2023-02-24 14:10:19 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo chmod -R 777 /home/ec2-user/SnapShot
-sudo cd /home/ec2-user/SnapShot && npm i
+sudo cd /home/ec2-user/SnapShot && npm config set legacy-peer-deps true && npm i 
 

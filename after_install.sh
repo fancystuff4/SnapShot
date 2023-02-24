80f09732b2b022ee5d567f24ef7f5f44bcb829bc
fancystuff4 <68031547+fancystuff4@users.noreply.github.com>
2023-02-24 16:32:08 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sudo mv -Rrp /home/node_modules /home/ec2-user/node_modules
+
 sudo chmod -R 777 /home/ec2-user
 
 #sudo cd /home/ec2-user && npm i /home/ec2-user
 
 cd /home/ec2-user
+
 npm -f install

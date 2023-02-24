8660cecb66a096e79812f9ecf2d84976e0dd2db3
fancystuff4 <68031547+fancystuff4@users.noreply.github.com>
2023-02-24 16:01:53 +0530

    Update application_stop.sh

--- application_stop.sh
+++ application_stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #stop existing file sample..
-sudo cd /home/ec2-user/
+cd /home/ec2-user/
 #sudo rm -rf /home/ec2-user/*.sh /home/ec2-user/*.png /home/ec2-user/*.md /home/ec2-user/*.yml /home/ec2-user/*.json /home/ec2-user/src /home/ec2-user/public 
-sudo rm -rf /home/ec2-user/*
-sudo rm -rf .g*
-sudo pm2 kill
+rm -rf /home/ec2-user/*
+rm -rf .g*
+pm2 kill

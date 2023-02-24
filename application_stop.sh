8faff8a08b9c6529e17a52e76eddd9c446594376
fancystuff4 <68031547+fancystuff4@users.noreply.github.com>
2023-02-24 12:46:20 +0530

    Update application_stop.sh

--- application_stop.sh
+++ application_stop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #stop existing file sample
 sudo cd /home/ec2-user/SnapShot/
-sudo npm cache clean --force
-sudo pm2 kill
+sudo cd /home/ec2-user/SnapShot/ && pm2 kill
 sudo rm -rf /home/ec2-user/SnapShot/*
 sudo rm -rf .g*
